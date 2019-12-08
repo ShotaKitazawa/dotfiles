6a921c793709c7aea272d6ef59e7deae96e9c21a
ShotaKitazawa <skitazawa1121@gmail.com>
2019-12-09 01:10:15 +0900

    install_packages.sh改修

--- README.md
+++ README.md
@@ -1,73 +1,31 @@
-# Usage
+# About
+
+Managed dotfiles & Install some softwares on multi-environments.
+
+# Execute
 
 * download
+
 ```bash
 git clone https://github.com/ShotaKitazawa/dotfiles $HOME/dotfiles
 ```
 
 * deploy dotfiles
+
 ```bash
 ./dotfiles/put_profile
 ```
 
 * source bashrc
+
 ```bash
 . ~/.bashrc_self
 ```
 
 * install applications
-    * requirements: yq
+
 ```bash
 ./dotfiles/install_packages
 ```
 
-# Memo
-
-## Install packages for some environments
-
-* Base package
-    * mac: `xcode-select --install`
-    * redhat: `yum install @base`
-    * debian: `apt install build-essential software-properties-common`
-* Repository
-    * mac: install homebrew: `/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)`
-    * redhat: `yum install epel-release`
-    * debian: `add-apt-repository ppa:neovim-ppa/unstable`
-* Xserver
-    * mac/linux: default ok
-    * wsl: download from https://sourceforge.net/projects/vcxsrv/ & install
-* Python
-    * install pyenv from https://github.com/pyenv/pyenv
-* NeoVim
-    * python3
-        * `pip install neovim`
-* Vim Plugin
-    * python3
-        * `pip install jedi flask autopep8`
-    * golang
-        * `go get -u github.com/nsf/gocode`
-        * `go get -u github.com/golang/lint/golint`
-    * C/C++
-        * mac: `brew install clang-format`
-        * redhat: TODO
-        * debian: `apt install clang-format`
-* mattn/memo
-    * peco
-        * install from https://github.com/peco/peco
-    * GoogleDrive ($HOME/GoogleDrive)
-        * for Linux: https://github.com/harababurel/gcsf
-* tmux
-    * shared clipboard
-        * mac: nothing required (tmux2.6+)
-        * wsl: download from https://github.com/equalsraf/win32yank/releases & deploy to $PATH
-        * redhat: `yum install xsel`
-        * debian: `apt install xsel`
-* Other
-    * mac
-        * cask: `brew install cask`
-        * macvim: `brew cask install macvim`
-        * pdf viewer: `brew cask install Skim`
-    * wsl
-        * /bin/open: `cmd.exe /c start $@`
-    * Windows (manually install)
-        * pdf viewer: https://www.sumatrapdfreader.org/download-free-pdf-viewer.html
+# Usage
--- install_packages.sh
+++ install_packages.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-set -xue -o pipefail
+set -u
 
-cd $(dirname $0) && pwd
+cd $(cd $(dirname $0) && pwd)
 
-LOGGER_STDOUT='/dev/null'
-LOGGER_STDERR='/dev/null'
+LOGGER_STDOUT=${LOGGER_STDOUT:-'/dev/null'}
+LOGGER_STDERR=${LOGGER_STDERR:-'/dev/null'}
 
-switch_env(){
+switch_env () {
   if [[ "$(uname)" = 'Darwin' ]]; then
     echo $2
   elif [[ "$(uname -r)" =~ ^.*-Microsoft$ ]]; then
@@ -19,151 +19,73 @@ switch_env(){
   fi
 }
 
-source ./requirements/git.sh
+_echo () {
+  echo $@ | tee -a $LOGGER_STDOUT | tee -a $LOGGER_STDERR
+}
 
 # create $HOME/bin
 if [ ! -e $HOME/bin ]; then mkdir $HOME/bin; fi
 
 # install required application: yq
 YQ_VERSION=2.3.0
-echo "> install yq" | tee $LOGGER_STDOUT
-curl -0L https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_$(switch_env linux darwin linux)_amd64 > $HOME/bin/yq 2> $LOGGER_STDERR
+_echo "### install yq ###"
+curl -s0L https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_$(switch_env linux darwin linux)_amd64 -o $HOME/bin/yq > $LOGGER_STDOUT 2> $LOGGER_STDERR
 chmod 0755 $HOME/bin/yq
-echo "-> installed" | tee $LOGGER_STDOUT
+_echo "-> installed"
 
+_echo '### download.yml ###'
+_file="./requirements/download.yml"
+for i in $(seq 0 $(( $(yq r $_file "[*].name" | wc -l) - 1 ))); do
+  _echo "-> install $(yq r $_file "[$i].name")"
+  case $(yq r $_file [$i].type) in
+    "text")
+      yq r $_file "[$i].contents" > $(eval echo $(yq r $_file [$i].path))
+      chmod $(yq r $_file "[$i].mode") $(eval echo $(yq r $_file [$i].path))
+      ;;
+    "clone")
+      git clone $(yq r $_file "[$i].url") -b $(yq r $_file "[$i].version") $(eval echo $(yq r $_file "[$i].path")) > $LOGGER_STDOUT 2> $LOGGER_STDERR
+      if [ "$(yq r $_file "[$i].option")" != "null" ]; then
+        eval $(yq r $_file "[$i].option") > $LOGGER_STDOUT 2> $LOGGER_STDERR
+      fi
+      ;;
+  esac
+done
 
-echo "> install kubectl-get_all" | tee $LOGGER_STDOUT
-cat <<'_EOF_' > $HOME/bin/kubectl-get_all
-#!/usr/bin/env bash
-set -e -o pipefail; [[ -n "$DEBUG" ]] && set -x
-exec kubectl get "$(kubectl api-resources --namespaced=true --verbs=list --output=name | tr "\n" "," | sed -e 's/,$//')" "$@"
-_EOF_
-chmod 0755 $HOME/bin/kubectl-get_all
-echo "-> installed" | tee $LOGGER_STDOUT
+_echo '### asdf.yml ###'
+_file="./requirements/asdf.yml"
+# TODO
 
-
-echo "> install peco" | tee $LOGGER_STDOUT
-curl -0L https://github.com/peco/peco/releases/download/${PECO_VERSION}/peco_$(switch_env linux darwin linux)_amd64.zip > /tmp/peco.zip 2> $LOGGER_STDERR
-cd /tmp
-  unzip /tmp/peco.zip
-  mv /tmp/peco_$(switch_env linux darwin linux)_amd64/peco $HOME/bin/peco
-  chmod 0755 $HOME/bin/peco
-cd -
-echo "-> installed" | tee $LOGGER_STDOUT
-
-echo "> install fzf" | tee $LOGGER_STDOUT
-if [ ! -d ~/.fzf ]; then
-  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf 2> $LOGGER_STDERR
-  git checkout $FZF_VERSINO
-  if ~/.fzf/install --all 2> $LOGGER_STDERR; then
-    echo "-> installed" | tee $LOGGER_STDOUT
-  else
-    echo "-> error: failed to download fzf" | tee $LOGGER_STDOUT
-  fi
-fi
-
-
-echo "> install pyenv" | tee $LOGGER_STDOUT
-if [ ! -d ~/.pyenv ]; then
-  git clone https://github.com/pyenv/pyenv ~/.pyenv 2> $LOGGER_STDERR
-  git checkout $PYENV_VERSION
-  export PYENV_ROOT=$HOME/.pyenv
-  export PATH=$PYENV_ROOT/bin:$PATH
-  eval "$(pyenv init -)"
-  echo "-> installed" | tee $LOGGER_STDOUT
-else
-  echo "-> pass" | tee $LOGGER_STDOUT
-fi
-
-
-# TODO: pyenv install -s が動かない > 手動インストールで対応
-echo "> install python packages" | tee $LOGGER_STDOUT
-if pyenv install -s $(cat ./requirements/language.yml | yq r - python.version) > $LOGGER_STDOUT 2> $LOGGER_STDERR; then
-  pyenv global $(cat ./requirements/language.yml | yq r - python.version)
-  for i in $(seq 0 $(( $(cat ./requirements/language.yml | yq r - python.packages | wc -l | awk '{print $1}') - 1 ))); do
-    echo "-> pip install -U $(cat ./requirements/language.yml | yq r - python.packages[$i])" | tee $LOGGER_STDOUT
-    pip install -U $(cat ./requirements/language.yml | yq r - python.packages[$i]) > $LOGGER_STDOUT 2> $LOGGER_STDERR
-  done
-fi
-
-
-echo "> install goenv" | tee $LOGGER_STDOUT
-if [ ! -d ~/.goenv ]; then
-  git clone https://github.com/syndbg/goenv ~/.goenv 2> $LOGGER_STDERR
-  git checkout $GOENV_VERSION
-  echo "-> installed" | tee $LOGGER_STDOUT
-else
-  echo "-> pass" | tee $LOGGER_STDOUT
-fi
-
-
-# TODO: goenv install -s が動かない > 手動インストールで対応
-echo "> install golang packages" | tee $LOGGER_STDOUT
-if goenv install -s $(cat ./requirements/language.yml | yq r - go.version) > $LOGGER_STDOUT 2> $LOGGER_STDERR; then
-  goenv global $(cat ./requirements/language.yml | yq r - go.version)
-  for i in $(seq 0 $(( $(cat ./requirements/language.yml | yq r - go.packages | wc -l | awk '{print $1}') - 1 ))); do
-    echo "-> go get -u $(cat ./requirements/language.yml | yq r - go.packages[$i])" | tee $LOGGER_STDOUT
-    go get -u $(cat ./requirements/language.yml | yq r - go.packages[$i]) > $LOGGER_STDOUT 2> $LOGGER_STDERR
-  done
-
-fi
-
-echo "> install peda (gdb extension)" | tee $LOGGER_STDOUT
-if [ ! -d ~/.peda ]; then
-  git clone https://github.com/longld/peda ~/.peda 2> $LOGGER_STDERR
-  git checkout $PEDA_VERSION
-  echo "-> installed" | tee $LOGGER_STDOUT
-else
-  echo "-> pass" | tee $LOGGER_STDOUT
-fi
-
-echo "> install Powerline fonts" | tee $LOGGER_STDOUT
-if [ ! -d ~/.fonts ]; then
-  git clone https://github.com/powerline/fonts.git ~/.fonts 2> $LOGGER_STDERR
-  git checkout $POWERLINE_FONT_VERSION
-  if ~/.fonts/install.sh > /dev/null; then
-    echo "-> installed" | tee $LOGGER_STDOUT
-  else
-    echo "-> error: failed to download Powerline fonts" | tee $LOGGER_STDOUT
-  fi
-else
-  echo "-> pass" | tee $LOGGER_STDOUT
-fi
-
-echo "> install Monaco Powerline fonts" | tee $LOGGER_STDOUT
-wget https://gist.github.com/baopham/1838072/raw/2c0e00770826e651d1e355962e751325edb0f1ee/Monaco%20for%20Powerline.otf -O $(switch_env $HOME/.local/share/fonts $HOME/Library/Fonts $HOME/.local/share/fonts)
-echo "-> installed" | tee $LOGGER_STDOUT
-
-echo "> for each OS"
-if [[ "$(uname)" = 'Darwin' ]]; then
+_echo '### package.yml ###'
+_file="./requirements/package.yml"
+if [[ "$(switch_env linux mac wsl)" = 'mac' ]]; then
   # for macOS
   if brew -v > $LOGGER_STDOUT 2> $LOGGER_STDERR; then
-    for i in $(seq 0 $(( $(cat ./requirements/os.yml | yq r - mac.brew | wc -l | awk '{print $1}') - 1 ))); do
-      echo "-> brew install $(cat ./requirements/os.yml | yq r - mac.brew[$i])" | tee $LOGGER_STDOUT
-      brew install $(cat ./requirements/os.yml | yq r - mac.brew[$i]) > $LOGGER_STDOUT 2> $LOGGER_STDERR
+    for i in $(seq 0 $(( $(yq r $_file "mac.brew" | wc -l) - 1 ))); do
+      _echo "-> brew install $(yq r $_file "mac.brew[$i]")"
+      brew install $(yq r $_file "mac.brew[$i]") > $LOGGER_STDOUT 2> $LOGGER_STDERR
     done
-    for i in $(seq 0 $(( $(cat ./requirements/os.yml | yq r - mac.cask | wc -l | awk '{print $1}') - 1 ))); do
-      echo "-> brew cask install $(cat ./requirements/os.yml | yq r - mac.cask[$i])" | tee $LOGGER_STDOUT
-      brew cask install $(cat ./requirements/os.yml | yq r - mac.cask[$i]) > $LOGGER_STDOUT 2> $LOGGER_STDERR
+    for i in $(seq 0 $(( $(yq r $_file "mac.cask" | wc -l) - 1 ))); do
+      _echo "-> brew cask install $(yq r $_file "mac.cask[$i]")"
+      brew cask install $(yq r $_file "mac.cask[$i]") > $LOGGER_STDOUT 2> $LOGGER_STDERR
     done
   fi
-elif [[ "$(uname)" = 'Linux' ]]; then
+elif [[ "$(switch_env linux mac wsl)" = 'linux' ]]; then
   if [ -e /etc/redhat-release ]; then
     # for CentOS & RHEL
-    for i in $(seq 0 $(( $(cat ./requirements/os.yml | yq r - redhat.yum | wc -l | awk '{print $1}') - 1 ))); do
-      echo "-> yum install -y $(cat ./requirements/os.yml | yq r - redhat.yum[$i])" | tee $LOGGER_STDOUT
-      yum install -y $(cat ./requirements/os.yml | yq r - redhat.yum[$i]) > $LOGGER_STDOUT 2> $LOGGER_STDERR
+    for i in $(seq 0 $(( $(yq r $_file "redhat.yum" | wc -l) - 1 ))); do
+      _echo "-> yum install -y $(yq r $_file "redhat.yum[$i]")"
+      yum install -y $(yq r $_file "redhat.yum[$i]") > $LOGGER_STDOUT 2> $LOGGER_STDERR
     done
     yum update > /dev/null 2>&1
-    for i in $(seq 0 $(( $(cat ./requirements/os.yml | yq r - redhat.yum-epel | wc -l | awk '{print $1}') - 1 ))); do
-      echo "-> yum --enablerepo=epel install -y $(cat ./requirements/os.yml | yq r - redhat.yum-epel[$i])" | tee $LOGGER_STDOUT
-      yum --enablerepo=epel install -y $(cat ./requirements/os.yml | yq r - redhat.yum-epel[$i]) > $LOGGER_STDOUT 2> $LOGGER_STDERR
+    for i in $(seq 0 $(( $(yq r $_file "redhat.yum-epel" | wc -l) - 1 ))); do
+      _echo "-> yum --enablerepo=epel install -y $(yq r $_file "redhat.yum-epel[$i]")"
+      yum --enablerepo=epel install -y $(yq r $_file "redhat.yum-epel[$i]") > $LOGGER_STDOUT 2> $LOGGER_STDERR
     done
   elif [ -e /etc/lsb-release ]; then
     # for Ubuntu
-    for i in $(seq 0 $(( $(cat ./requirements/os.yml | yq r - redhat.yum | wc -l | awk '{print $1}') - 1 ))); do
-      echo "-> apt install -y $(cat ./requirements/os.yml | yq r - ubuntu.apt[$i])" | tee $LOGGER_STDOUT
-      apt install -y $(cat ./requirements/os.yml | yq r - ubuntu.apt[$i]) > $LOGGER_STDOUT 2> $LOGGER_STDERR
+    for i in $(seq 0 $(( $(yq r $_file "ubuntu.apt" | wc -l) - 1 ))); do
+      _echo "-> apt install -y $(yq r $_file "ubuntu.apt[$i]")"
+      apt install -y $(yq r $_file "ubuntu.apt[$i]") > $LOGGER_STDOUT 2> $LOGGER_STDERR
     done
   fi
 fi
